ae2b8a7c490ec2d20248dfdb424084efdf6edc26
Joan Gamell <gamell@gmail.com>
2016-04-04 00:00:49 -0700

    Initial changes to add additional options, more ES2015 syntax, some re-factoring and including ESlint

--- index.js
+++ index.js
@@ -1,55 +1,71 @@
+'use strict';
+
 const marked = require('marked');
 const fs = require('fs');
 const pathResolve = require('path').resolve;
+const defaults = require('defaults');
 
-const commentReg = /^\s*<!--\s*(.*?)\s*-->\s*$/gm;
-var getMetaData = (content) => {
-    commentReg.lastIndex = 0;
-    var match = commentReg.exec(content);
-    if (!match) {
-        return '';
-    }
-    var metaData = [];
-    var metaArr = match[1].split(/\s+/);
-    metaArr.forEach(function (meta) {
-        var kv = meta.split('=');
-        metaData.push([
-            kv[0].replace(/^data-/, ''),
-            kv[1].replace(/^('|")?(.*?)\1$/, '$2')
-        ]);
+const commentRegex = /^\s*<!--\s*(.*?)\s*-->\s*$/gm;
+const slideSeparatorRegex = /^-{6,}$/m;
+
+function getLayoutData(content) {
+  commentRegex.lastIndex = 0;
+  const match = commentRegex.exec(content);
+  if (!match) {
+    return '';
+  }
+  const metaData = [];
+  const metaArr = match[1].split(/\s+/);
+  metaArr.forEach((meta) => {
+    const kv = meta.split('=');
+    metaData.push({
+      name: kv[0].replace(/^data-/, ''),
+      value: kv[1].replace(/^('|")?(.*?)\1$/, '$2'),
     });
-    return metaData.map(function (meta) {
-        return 'data-' + meta[0] + '="' + meta[1] + '"';
-    }).join(' ');
-};
+  });
+  return metaData.map((meta) => `data-${meta.name}="${meta.value}"`).join(' ');
+}
 
-var createSlideDiv = (content) =>
-     `<div class="step" ${getMetaData(content)}>${marked(content)}</div>`;
+function createSlideHtml(content) {
+  return `<div class="step" ${getLayoutData(content)}>${marked(content)}</div>`;
+}
 
-var readFile = (path) => {
-    path = pathResolve(__dirname, path);
-    return String(fs.readFileSync(path));
-};
+function readFile(pathArg) {
+  const path = pathResolve(__dirname, pathArg);
+  return String(fs.readFileSync(path));
+}
 
-var createImpressHTML = (html) => {
-    var tpl = readFile('./res/impress.tpl');
-    var data = {
-        html: html,
-        css: readFile('./res/impress.css'),
-        js: readFile('./res/impress.min.js')
-    };
-    return tpl.replace(/\{\{\$(\w+)\}\}/g, function ($, $1) {
-        return data[$1];
-    });
-};
+function createImpressHTML(html) {
+  const tpl = readFile('./res/impress.tpl');
+  const data = {
+    html,
+    css: readFile('./res/impress.css'),
+    js: readFile('./res/impress.min.js'),
+  };
+  return tpl.replace(/\{\{\$(\w+)\}\}/g, ($, $1) => data[$1]);
+}
 
-var processMarkdownFile = (path) => {
-    var contentArr = String(fs.readFileSync(path)).split(/^-{6,}$/m);
-    var html = '';
-    contentArr.forEach(function (content) {
-        html += createSlideDiv(content);
-    });
-    return createImpressHTML(html);
-};
+function containsLayoutData(markdown) {
+  return markdown.search(commentRegex) !== -1;
+}
+
+function processMarkdownFile(path, optionsArg) {
+  const options = defaults(optionsArg, {
+    layout: 'horizontal',
+    theme: 'light',
+  });
+  const markdown = String(fs.readFileSync(path));
+  // disable auto layout if custom metadata is found
+  if (containsLayoutData(markdown)) {
+    console.log('contains metadata!');
+    options.layout = 'custom';
+  }
+  const slides = markdown.split(slideSeparatorRegex);
+  let html = '';
+  slides.forEach((content) => {
+    html += createSlideHtml(content);
+  });
+  return createImpressHTML(html);
+}
 
 module.exports = processMarkdownFile;
--- markpress.js
+++ markpress.js
@@ -1,35 +1,51 @@
 #! /usr/bin/env node
 
+'use strict';
+
 const markpress = require('./index.js');
 const program = require('commander');
 const path = require('path');
 const fs = require('fs');
 const pkg = require('./package');
 
+const layoutRegex = /^(horizontal|vertical|3d|random)$/i;
+const themeRegex = /^(light|dark)$/i;
+
 program.version(pkg.version)
     .option('-i, --input <path>', 'Input markdown file path')
     .option('-o, --output <path>', 'Impress htmll file output path')
-    .option('-l, --layout [horizontal|vertical|3d|random]', 'Chose the impress.js layout. If any specific coordinates/scale metadata is found on any individual slide, this option will be ignored')
-    .option('-s, --style [light (default)|dark]')
+    .option(
+      '-l, --layout <layout>',
+      'Chose the impress.js layout [horizontal (default)|vertical|3d|random]',
+      layoutRegex,
+      'horizontal'
+    )
+    .option(
+      '-t, --theme <theme>',
+      'Chose the theme of colors [light (default)|dark]',
+      themeRegex,
+      'light'
+    )
     //.option('-b, --build [compact (default)|expanded], 'Chose if the output will be a single html file or an HTML file and CSS files for easy editing.')
-    .on('--help', function () {
-        console.log('  Examples:');
-        console.log('');
-        console.log('    $ markpress -i file.md -o file.html -l random -s dark');
-        console.log('');
+    .on('--help', () => {
+      console.log('  Examples:\n');
+      console.log('    $ markpress -i file.md -o file.html -l random -t dark\n');
     })
     .parse(process.argv);
 
 if (!program.input || !program.output) {
-    console.log('');
-    console.log('  Must have input and output arg!');
-    program.help();
-    process.exit();
+  console.log('\nError: Must have input and output arg!');
+  program.help();
+  process.exit();
 }
 
 const basePath = process.cwd();
-var input = path.resolve(basePath, program.input);
-var output = path.resolve(basePath, program.output);
+const input = path.resolve(basePath, program.input);
+const output = path.resolve(basePath, program.output);
+const options = {
+  layout: program.layout,
+  style: program.style,
+};
 
-var html = markpress(input);
-fs.writeFileSync(output, html);
+const impressHtml = markpress(input, options);
+fs.writeFileSync(output, impressHtml);
